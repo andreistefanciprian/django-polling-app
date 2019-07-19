841dc98fcbe695450ee37e86393d64d91291dede
Stefan Andrei <stefan.andrei@virginmedia.co.uk>
2019-07-19 09:38:36 +0100

    add sample views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),        # /polls/
+    path('<int:question_id>/', views.detail, name='detail'),        # /polls/11/
+    path('<int:question_id>/results/', views.results, name='results'),      # /polls/11/results/
+    path('<int:question_id>/vote/', views.vote, name='vote'),       # /polls/11/vote/
 ]
